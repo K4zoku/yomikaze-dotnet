8dfd08d698930cb09ad38464579cd00f5cd860ac
Kazoku <k4zoku@pm.me>
2024-03-16 20:34:01 +0700

    Added Login Page

---
+++ Presentation/Web/Pages/Login.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- Presentation/Web/wwwroot/js/site.js
+++ Presentation/Web/wwwroot/js/site.js
@@ -1,4 +1,87 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('alpine:init', () => {
+    console.log("Alpine init")
+    Alpine.store('auth', {
+        token: localStorage.getItem('token'),
+        http: null,
 
-// Write your JavaScript code.
+        init() {
+            this.http = axios.create({
+                baseURL: '/',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': this.token ? `Bearer ${this.token}` : undefined
+                }
+            });
+            console.log("Auth init")
+        },
+        
+        get authenticated() {
+            return !!this.token;
+        },
+        
+        getProfile() {
+            return this.authenticated ? this.http.get('/API/Authenticate/Info') : Promise.reject('Not authenticated');
+        },
+
+        login(token) {
+            this.token = token
+            localStorage.setItem('token', token);
+            this.http.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        },
+
+        logout() {
+            this.token = null
+            localStorage.removeItem('token');
+            this.http.defaults.headers.common['Authorization'] = undefined;
+            window.location.reload();
+        },
+    });
+    
+    Alpine.data('theme', () => ({
+        init() {
+            this.$watch('id', (value) => localStorage.setItem('theme', value))
+        },
+        id: parseInt(localStorage.getItem('theme')) || 0,
+        themes: [
+            {
+                theme: 'ayu-light',
+                name: 'Ayu Light',
+                dark: false,
+                icon: 'sun',
+            },
+            {
+                theme: 'ayu-mirage',
+                name: 'Ayu Mirage',
+                dark: true,
+                icon: 'sunset',
+            },
+            {
+                theme: 'ayu-dark',
+                name: 'Ayu Dark',
+                dark: true,
+                icon: 'moon',
+            }
+        ],
+        nextTheme() {
+            this.id = (this.id + 1) % this.themes.length;
+        },
+        get currentTheme() {
+            return this.themes[this.id]
+        },
+        get theme() {
+            return this.currentTheme.theme
+        },
+        get isDark() {
+            return this.currentTheme.dark
+        },
+        get themeIcon() {
+            return this.currentTheme.icon
+        }
+    }));
+});
+
+document.addEventListener('alpine:initialized', () => {
+    console.log("Alpine initialized")
+})
+
