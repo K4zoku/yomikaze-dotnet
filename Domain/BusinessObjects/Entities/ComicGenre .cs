c2be70d31fc6185f03c3964ab8b32796fc246ef3
Radkaim <hungct04@gmail.com>
2024-03-16 22:04:33 +0700

    add comicgenre

---
+++ Domain/BusinessObjects/Entities/ComicGenre .cs
@@ -0,0 +1,16 @@
+﻿using Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yomikaze.Domain.Entities;
+public class ComicGenre : BaseEntity
+{
+    public virtual long ComicId { get; set; }
+    public virtual Comic Comic { get; set; } = default!;
+
+    public virtual long GenreId { get; set; }
+    public virtual Genre Genre { get; set; } = default!;
+}
---
+++ Domain/BusinessObjects/Models/ComicGenreModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yomikaze.Domain.Models;
+public class ComicGenreInputModel
+{
+    public long GenreId { get; set; }
+}
+
+public class ComicGenreOutputModel
+{
+    public GenreOutputModel Genres { get; set; } = default!;
+}
