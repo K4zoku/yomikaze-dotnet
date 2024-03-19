920b2a7cf1bb4d72bbe17cb56f19e5a4af28d509
Radkaim <hungct04@gmail.com>
2024-03-19 19:38:24 +0700

    add chapter can read chapter

--- Presentation/Web/Pages/Comics/Chapter.cshtml.cs
+++ Presentation/Web/Pages/Comics/Chapter.cshtml.cs
@@ -4,7 +4,13 @@ namespace Yomikaze.Presentation.Web.Pages.Comics;
 
 public class ChapterModel : PageModel
 {
-    public void OnGet()
+
+    public string ComicId { get; set; } = string.Empty;
+    public int Index { get; set; } = 0;
+
+    public void OnGet(string comicId, int index)
     {
+        this.ComicId = comicId;
+        Index = index;
     }
 }
