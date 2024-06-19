688696fba0a68b4739fe3d0f41c1b809eff15628
Kazoku <k4zoku@pm.me>
2024-06-20 01:26:35 +0700

    Fixed warnings

--- Domain/Yomikaze.Domain.Identity/Models/UserModel.cs
+++ Domain/Yomikaze.Domain.Identity/Models/UserModel.cs
@@ -17,7 +17,7 @@ public class UserInputModel
 
 public class UserOutputModel
 {
-    public string Id { get; set; }
+    public string Id { get; set; } = default!;
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? Fullname { get; set; }
