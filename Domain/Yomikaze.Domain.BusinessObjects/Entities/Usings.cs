4ce87e9651eb777a96ff757a4912a5f55e7bcc41
Kazoku <k4zoku@pm.me>
2024-06-28 22:45:03 +0700

    [Deploy] Remove duplicated usings

--- Domain/Yomikaze.Domain.BusinessObjects/Entities/Usings.cs
+++ Domain/Yomikaze.Domain.BusinessObjects/Entities/Usings.cs
@@ -3,5 +3,4 @@ global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using Yomikaze.Domain.Abstracts;
 global using System.Runtime.Serialization;
-global using Yomikaze.Domain.Abstracts;
 global using Microsoft.AspNetCore.Identity;
