9acca18bd9930159c6220a0cd23e70aceb0ec2fa
Kazoku <k4zoku@pm.me>
2024-07-16 17:06:21 +0700

    Include author roles when getting comment

--- Application/Yomikaze.Application/Data/Repos/ComicCommentRepository.cs
+++ Application/Yomikaze.Application/Data/Repos/ComicCommentRepository.cs
@@ -4,6 +4,11 @@ namespace Yomikaze.Application.Data.Repos;
 
 public class ComicCommentRepository(DbContext dbContext) : BaseRepository<ComicComment>(new ComicCommentDao(dbContext)) 
 {
+    public override IQueryable<ComicComment> Query()
+    {
+        return base.Query().Include(x => x.Author).ThenInclude(x => x.Roles);
+    }
+
     public IQueryable<ComicComment> GetByComicId(ulong comicId)
     {
         return Query().Where(x => x.ComicId == comicId);
