219bfd2a8ab04865966d795daac71d2552905c6c
Kazoku <k4zoku@pm.me>
2024-07-18 15:02:06 +0700

    [Deploy] Return Body ?

--- Presentation/API/ImagesAPI/Program.cs
+++ Presentation/API/ImagesAPI/Program.cs
@@ -67,7 +67,12 @@ app.Use(async (context, next) => {
     await next.Invoke();
     if (context.Response.StatusCode == (int)HttpStatusCode.NotFound)
     {
-        context.Response.Redirect("/images/default.webp");
+        var defaultFile = fileProvider.GetFileInfo("misc/default.webp");
+        context.Response.ContentType = "image/webp";
+        await using Stream stream = defaultFile.CreateReadStream();
+        await stream.CopyToAsync(context.Response.Body);
+        await context.Response.StartAsync();
+        // context.Response.Redirect("/images/misc/default.webp");
     }
 });
 app.UseStaticFiles(new StaticFileOptions
