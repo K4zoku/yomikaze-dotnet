d7fb0ceab86996606f06c71fcde75d8c389c5d27
Kazoku <k4zoku@pm.me>
2024-07-30 19:20:27 +0700

    [Deploy] Update chart

--- Presentation/API/YomikazeAPI/Controllers/StatisticsController.cs
+++ Presentation/API/YomikazeAPI/Controllers/StatisticsController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.Serialization;
 using Yomikaze.Infrastructure.Context;
 
 namespace Yomikaze.API.Main.Controllers;
@@ -7,7 +9,6 @@ namespace Yomikaze.API.Main.Controllers;
 [Authorize(Roles = "Super,Administrator,Publisher")]
 public class StatisticsController(ILogger<StatisticsController> logger, YomikazeDbContext context) : ControllerBase
 {
-
     [HttpGet]
     public ActionResult GetStatistics()
     {
@@ -21,10 +22,10 @@ public class StatisticsController(ILogger<StatisticsController> logger, Yomikaze
         var reports = context.ChapterReports.Count() + context.ComicReports.Count() + context.ProfileReports.Count() +
                       context.Set<CommentReport>().Count();
         var comments = context.ChapterComments.Count() + context.ComicComments.Count();
-        
+
         var income = context.Transactions.Where(x => x.Type == TransactionType.PurchaseCoin).Sum(x => x.Amount);
         var outcome = context.Transactions.Where(x => x.Type == TransactionType.Withdrawal).Sum(x => x.Amount);
-        
+
         return Ok(new
         {
             comics,
@@ -46,28 +47,69 @@ public class StatisticsController(ILogger<StatisticsController> logger, Yomikaze
     {
         var comics = context.Comics
             .GroupBy(x => x.CreationTime)
-            .Select(x => new {x = x.Key, y = x.Count()})
+            .Select(x => new { x = x.Key, y = x.Count() })
+            .OrderBy(x => x.x)
+            .AsEnumerable()
+            .GroupBy(x => x.x?.ToString("yyyy-MM-dd"))
+            .Select(x => new { x = x.Key, y = x.Sum(y => y.y) })
             .OrderBy(x => x.x)
             .ToList();
-        return Ok(comics); 
+        // accumulate
+        var dataset = new
+        {
+            Data = comics.Aggregate(new List<int>(), (acc, x) =>
+            {
+                if (acc.Count == 0)
+                {
+                    acc.Add(x.y);
+                }
+                else
+                {
+                    acc.Add(acc[^1] + x.y);
+                }
+
+                return acc;
+            }),
+        };
+        // add date between with count = date before
+        var format = new DateTimeFormat("yyyy-MM-dd");
+        var start = DateTime.Parse(comics[0].x!, format.FormatProvider);
+        var end = DateTime.Parse(comics[^1].x!, format.FormatProvider);
+        var date = start;
+        var index = 0;
+        var labels = new List<string>();
+        var data = new List<int>();
+        while (date < end)
+        {
+            labels.Add(date.ToString("yyyy-MM-dd"));
+            data.Add(dataset.Data[index]);
+            date = date.AddDays(1);
+            var next = DateTime.Parse(comics[index].x!, format.FormatProvider);
+            Console.Out.WriteLine("date: {0}, next {1}", date, next);           
+            if (date >= next)
+            {
+                index++;
+            }
+        }   
+        var chart = new { Labels = labels, Datasets = new ArrayList { new { Data = data, } } };
+        return Ok(chart);
     }
-    
+
     [HttpGet("financial/chart")]
     public ActionResult GetFinancialCharts()
     {
         var income = context.Transactions
             .Where(x => x.Type == TransactionType.PurchaseCoin)
             .GroupBy(x => x.CreationTime)
-            .Select(x => new {x = x.Key, y = x.Sum(y => y.Amount)})
+            .Select(x => new { x = x.Key, y = x.Sum(y => y.Amount) })
             .OrderBy(x => x.x)
             .ToList();
         var outcome = context.Transactions
             .Where(x => x.Type == TransactionType.Withdrawal)
             .GroupBy(x => x.CreationTime)
-            .Select(x => new {x = x.Key, y = x.Sum(y => y.Amount)})
+            .Select(x => new { x = x.Key, y = x.Sum(y => y.Amount) })
             .OrderBy(x => x.x)
             .ToList();
-        return Ok(new {income, outcome});
+        return Ok(new { income, outcome });
     }
-    
 }
